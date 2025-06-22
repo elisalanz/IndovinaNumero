57ffe4eb2103fcb3375c2a5c0c2b0256ede62a8a
elisalanzi <elisa.lanzillotti.2003@gmail.com>
2025-06-22 18:03:11 +0200

    effettuate modifiche

--- controller.py
+++ controller.py
@@ -5,3 +5,58 @@ class Controller(object):
     def __init__(self, view: View):
         self._view = view
         self._model = Model()
+
+    def reset(self, e):
+        self._model.reset()
+        self._view._txtOutT.value = self._model.T
+        self._view._lv.controls.clear()
+        self._view._txtIn.disabled = False
+        self._view._btnPlay.disabled = False
+        self._view._lv.controls.append(ft.Text("Indovina a quale numero sto pensando!"))
+        self._view.update()
+
+    def play(self, e):
+        tentativoStr = self._view._txtIn.value
+        self._view._txtIn.value = ""
+        self._view._txtOutT.value = self._model.T -1
+        if tentativoStr == "":
+            self._view._lv.controls.append(ft.Text("Attenzione! Inserisci un valore numerico da testare", color = "red"))
+            self._view.update()
+            return
+        try:
+            tentativoInt = int(tentativoStr)
+        except ValueError:
+            self._view._lv.controls.append(ft.Text("Attenzione! Il valore inserito non è un intero", color="red"))
+            self._view.update()
+            return
+
+        res  = self._model.play(tentativoInt)
+        if res == 0: # ho vinto
+            self._view._lv.controls.append(ft.Text(f"Fantastico! Hai vinto, il segreto era {tentativoInt}", color="green"))
+            self._view._btnPlay.disabled = True
+            self._view._txtIn.disabled = True
+            self._view.update()
+            return
+        elif res == 2: # ho finito tutte le vite
+            self._view._lv.controls.append(ft.Text(f"Mi dispiace, hai finito le vite, il segreto era {self._model.segreto}", color="red"))
+            self._view._btnPlay.disabled = True
+            self._view._txtIn.disabled = True
+            self._view.update()
+            return
+        elif res == -1: # il mio segreto è più piccolo
+            self._view._lv.controls.append(ft.Text(f"Il segreto è più piccolo di {tentativoInt}"))
+            self._view.update()
+            return
+        else:
+            self._view._lv.controls.append(ft.Text(f"Il segreto è più grande di {tentativoInt}"))
+            self._view.update()
+            return
+
+    def getNMax(self):
+        return self._model.NMax
+
+    def getTMax(self):
+        return self._model.TMax
+
+
+
--- view.py
+++ view.py
@@ -12,6 +12,22 @@ class View(object):
         self._titolo = ft.Text("Indovina il numero",
                                color="blue", size=24)
 
+        self._txtOutNMax = ft.TextField(label= "N Max", disabled = True, width = 200, value = self._controller.getNMax())
+        self._txtOutTMax = ft.TextField(label= "T Max", disabled = True, width = 200, value = self._controller.getTMax())
+        self._txtOutT = ft.TextField(label= "T rimanenti", disabled = True, width = 200)
+
+        self._txtIn = ft.TextField(label= "Tentativo", width = 200, disabled = True)
+        self._btnReset = ft.ElevatedButton(text = "Nuova Partita", width = 200, on_click = self._controller.reset)
+        self._btnPlay = ft.ElevatedButton(text = "Gioca", width = 200, on_click = self._controller.play, disabled = True)
+
+        self._lv = ft.ListView(expand = True)  # expand = 1
+
+        row1 = ft.Container(self._titolo, alignment = ft.alignment.center)
+        row2 = ft.Row([self._txtOutNMax, self._txtOutTMax, self._txtOutT], alignment = ft.MainAxisAlignment.CENTER)
+        row3 = ft.Row([self._btnReset, self._txtIn, self._btnPlay], alignment = ft.MainAxisAlignment.CENTER)
+
+        self._page.add(row1, row2, row3, self._lv)
+
         self._page.update()
 
     def setController(self,controller):
